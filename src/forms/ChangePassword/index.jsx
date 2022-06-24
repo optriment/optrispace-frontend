c9b445a66c685d66ad03c867d0d2827f7f790a88
Alexander Kadyrov <gruz0.mail@gmail.com>
2022-06-24 12:34:44 +0300

    Add change password

---
+++ src/forms/ChangePassword/index.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import { useRouter } from 'next/router'
+import { Button, Form, Segment } from 'semantic-ui-react'
+import ErrorWrapper from '../../components/ErrorWrapper'
+import { changeUserPassword } from '../../lib/settings'
+
+export default function ChangePassword({ token, authenticate }) {
+  const router = useRouter()
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const initialFields = {
+    old_password: '',
+    new_password: '',
+  }
+
+  const [fields, setFields] = useState(initialFields)
+  const [error, setError] = useState(undefined)
+
+  const handleChangePassword = async (e) => {
+    e.preventDefault()
+
+    setError(null)
+    setIsSubmitting(true)
+
+    try {
+      const json = await changeUserPassword(token, {
+        old_password: fields.old_password,
+        new_password: fields.new_password,
+      })
+
+      await authenticate(json.token)
+
+      router.reload()
+    } catch (error) {
+      console.error({ error })
+      setError(error)
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  const handleInputChange = (e) => {
+    setFields({ ...fields, ...{ [e.target.id]: e.target.value } })
+  }
+
+  return (
+    <>
+      {error && (
+        <ErrorWrapper header="Unable to change password" error={error} />
+      )}
+
+      <Form size="large" onSubmit={handleChangePassword}>
+        <Segment>
+          <Form.Input
+            id="old_password"
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Current Password"
+            type="password"
+            required
+            autoComplete="current-password"
+            value={fields.old_password}
+            onChange={handleInputChange}
+          />
+
+          <Form.Input
+            id="new_password"
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="New Password"
+            type="password"
+            required
+            autoComplete="new-password"
+            value={fields.new_password}
+            onChange={handleInputChange}
+          />
+
+          <Button primary fluid size="large" disabled={isSubmitting}>
+            Update Password
+          </Button>
+        </Segment>
+      </Form>
+    </>
+  )
+}
