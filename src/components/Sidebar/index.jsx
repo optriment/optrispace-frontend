075ae0cd2490699edb318a395ccf7da01c8f5b79
Alexander Kadyrov <gruz0.mail@gmail.com>
2022-07-12 13:25:45 +0300

    Send marketing forms to Telegram

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,53 +1,21 @@
 import React from 'react'
-import {
-  Form,
-  Checkbox,
-  Segment,
-  Divider,
-  Button,
-  Header,
-} from 'semantic-ui-react'
+import { Segment, Button, Header } from 'semantic-ui-react'
+import SidebarRegistrationForm from '../../forms/Marketing/SidebarRegistrationForm'
 
 const Sidebar = () => {
   return (
     <>
       <Segment>
         <Header as="h3">Join our newsletter!</Header>
-        <Form>
-          <Form.Field>
-            <input placeholder="Your Email" required />
-          </Form.Field>
-          <Form.Field>
-            <label>What sort of jobs are you interested in:</label>
-          </Form.Field>
-          <Form.Field>
-            <Checkbox label="Software Development" />
-          </Form.Field>
-          <Form.Field>
-            <Checkbox label="Design/Illustrations" />
-          </Form.Field>
-          <Form.Field>
-            <Checkbox label="Content Management" />
-          </Form.Field>
-          <Form.Field>
-            <Checkbox label="Copywriting" />
-          </Form.Field>
-          <Form.Field>
-            <Checkbox label="Translations" />
-          </Form.Field>
-          <Form.Field>
-            <Checkbox label="PM/PO" />
-          </Form.Field>
-          <Form.Field>
-            <Checkbox label="Other" />
-          </Form.Field>
 
-          <Divider />
+        <p>
+          Want to be notified when new jobs and tasks comes out? Simply drop
+          your email in the form below.
+        </p>
 
-          <Button primary type="submit">
-            Get Updates!
-          </Button>
-        </Form>
+        <p>We will not spam you. Unsubscribe any time.</p>
+
+        <SidebarRegistrationForm />
       </Segment>
 
       <Segment>
