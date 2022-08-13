c3dd6d41c1264b63f939d30cd6ac2e84df6756f3
Alexander Kadyrov <gruz0.mail@gmail.com>
2022-08-13 14:06:39 +0300

    Replace feedback buttons with usersnap.com

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,43 +1,12 @@
 import React from 'react'
-import { useRouter } from 'next/router'
-import { Button, Divider, Header, Segment } from 'semantic-ui-react'
+import { Segment } from 'semantic-ui-react'
 
 const Footer = () => {
-  const router = useRouter()
-
-  if (
-    router.pathname.startsWith('/sign_in') ||
-    router.pathname.startsWith('/sign_up') ||
-    router.pathname === '/'
-  ) {
-    return null
-  }
-
   return (
     <>
-      <Divider />
-
-      <Segment basic>
-        <Segment basic textAlign="center">
-          <Header as="h2">
-            If you have any questions please click the button below
-          </Header>
-
-          <Divider hidden />
-
-          <Button
-            as="a"
-            content="Leave feedback"
-            icon="write"
-            positive
-            href="https://forms.gle/EjLSGHEpY4eafUr89"
-            target="_blank"
-            rel="noopener noreferrer"
-          />
-        </Segment>
+      <Segment basic padded textAlign="center">
+        &copy; 2022 Optriment
       </Segment>
-
-      <Divider hidden />
     </>
   )
 }
