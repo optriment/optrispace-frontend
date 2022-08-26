ae506b2aa61a69b2e382691434d6f78c7e7289ac
Alexander Kadyrov <gruz0.mail@gmail.com>
2022-08-26 15:00:22 +0300

    Add FAQ to landing page

--- src/screens/landing/index.jsx
+++ src/screens/landing/index.jsx
@@ -43,7 +43,95 @@ export const LandingScreen = ({ isSmallScreen }) => {
         </Button.Group>
       </Container>
 
-      <Divider hidden style={{ marginTop: isSmallScreen ? '2em' : '5em' }} />
+      <Divider style={{ marginTop: isSmallScreen ? '2em' : '5em' }} />
+
+      <Container text>
+        <Header
+          as="h2"
+          content="What's going on?"
+          style={{
+            fontSize: isSmallScreen ? '1.5em' : '1.8em',
+            fontWeight: 'normal',
+            marginTop: isSmallScreen ? '1.0em' : '1.3em',
+          }}
+        />
+
+        <p>
+          We are OptriSpace — a platform for freelancers looking for jobs and
+          for customers looking for professionals for their projects. Besides
+          that all our users will become shareholders of our company.
+        </p>
+
+        <p>
+          Our platform is based on the blockchain technology and uses
+          cryptocurrency as a payment method. That makes OptriSpace secure and
+          fast. We&apos;re almost ready to launch the beta testing and we need
+          you to share your user experience to improve our product.
+        </p>
+
+        <Header
+          as="h2"
+          content="Discover how to earn more"
+          style={{
+            fontSize: isSmallScreen ? '1.5em' : '1.8em',
+            fontWeight: 'normal',
+            marginTop: isSmallScreen ? '1.0em' : '1.3em',
+          }}
+        />
+
+        <p>
+          We are looking for hardworking and independent talents who seek global
+          contracts with less paperwork and more freedom. We offer a smart
+          contracting platform to connect with customers and experts.
+        </p>
+
+        <Header
+          as="h2"
+          content="Who can find job on the platform right now?"
+          style={{
+            fontSize: isSmallScreen ? '1.5em' : '1.8em',
+            fontWeight: 'normal',
+            marginTop: isSmallScreen ? '1.0em' : '1.3em',
+          }}
+        />
+
+        <p>
+          Software Developers, QA Engineers, Designers, Copywriters,
+          Translators.
+        </p>
+
+        <Header
+          as="h2"
+          content="What network do we use?"
+          style={{
+            fontSize: isSmallScreen ? '1.5em' : '1.8em',
+            fontWeight: 'normal',
+            marginTop: isSmallScreen ? '1.0em' : '1.3em',
+          }}
+        />
+
+        <p>
+          During the alpha test we are using Binance Smart Chain (Testnet).
+          <br />
+          Our main currency is native currency of this network (Test BNB).
+        </p>
+
+        <Header
+          as="h2"
+          content="What about payments?"
+          style={{
+            fontSize: isSmallScreen ? '1.5em' : '1.8em',
+            fontWeight: 'normal',
+            marginTop: isSmallScreen ? '1.0em' : '1.3em',
+          }}
+        />
+
+        <p>
+          All payments will be in crypto.
+          <br />
+          Additional information you will receive after the registration.
+        </p>
+      </Container>
     </>
   )
 }
