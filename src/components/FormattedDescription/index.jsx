2ebdf2e4f095aa306c2162aec6deaa6879837433
e.leurdo <e.leurdo@yadro.com>
2022-10-22 14:37:20 +0300

    markdown parser added to job card, contract card, chat message

--- src/components/FormattedDescription/index.jsx
+++ src/components/FormattedDescription/index.jsx
@@ -1,16 +1,10 @@
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
 export const FormattedDescription = ({ description }) => {
   return (
     <div style={{ textAlign: 'justify' }}>
-      {description
-        .trim()
-        .split('\n')
-        .map((str, idx) => (
-          <div key={idx}>
-            {str}
-
-            <br />
-          </div>
-        ))}
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>{description}</ReactMarkdown>
     </div>
   )
 }
