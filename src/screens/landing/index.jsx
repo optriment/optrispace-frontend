3309f026eff38257b83e0dd908795858853a7608
Alexander Kadyrov <gruz0.mail@gmail.com>
2022-10-31 21:32:35 +0400

    Update Landing content

--- src/screens/landing/index.jsx
+++ src/screens/landing/index.jsx
@@ -20,13 +20,16 @@ export const LandingScreen = ({ isSmallScreen }) => {
 
         <Header
           as="h2"
-          content="Find a Job. Find a Pro."
           style={{
             fontSize: isSmallScreen ? '1.3em' : '1.8em',
             fontWeight: 'normal',
             marginTop: isSmallScreen ? '0.8em' : '1.3em',
           }}
-        />
+        >
+          No middlemen. No paperwork.
+          <br />
+          Fast & Secure Payments.
+        </Header>
 
         <Divider style={{ marginTop: isSmallScreen ? '1em' : '3em' }} hidden />
 
@@ -48,7 +51,7 @@ export const LandingScreen = ({ isSmallScreen }) => {
       <Container text textAlign="justified">
         <Header
           as="h2"
-          content="What is going on?"
+          content="What is OptriSpace"
           style={{
             fontSize: isSmallScreen ? '1.5em' : '1.8em',
             fontWeight: 'normal',
@@ -66,7 +69,74 @@ export const LandingScreen = ({ isSmallScreen }) => {
         <p>
           Our platform is based on the blockchain technology and uses
           cryptocurrency as a payment method. That makes OptriSpace secure and
-          fast.
+          fast. Smart Contracts protect customers and freelancers from third
+          parties and middlemen. Smart Contracts handle all logic depends on
+          terms and conditions which have to be signed by both participants.
+        </p>
+
+        <Header
+          as="h2"
+          content="How OptriSpace works"
+          style={{
+            fontSize: isSmallScreen ? '1.5em' : '1.8em',
+            fontWeight: 'normal',
+            marginTop: isSmallScreen ? '1.0em' : '1.3em',
+          }}
+        />
+
+        <Header as="h3" content="1. Customer posts a job on the platform" />
+
+        <p>
+          A customer lists a particular task on the platform and picks a person
+          who wants to work with upon this task. Both of them use our internal
+          messaging system to discuss terms and conditions (price, acceptance
+          criteria, etc.) of a future contract.
+        </p>
+
+        <Header
+          as="h3"
+          content="2. Freelancer agrees to terms, accepts and signs the contract"
+        />
+
+        <p>
+          At this stage the freelancer has to accept the contract on our
+          platform.
+          <br />
+          After that the customer will be able to create a Smart Contract on
+          blockchain.
+        </p>
+
+        <Header as="h3" content="3. Customer funds the Smart Contract" />
+
+        <p>
+          The customer initiates transaction on blockchain to send money from
+          own wallet to Smart Contract address. The funds are held on the Smart
+          Contract address until all conditions have been met.
+        </p>
+
+        <Header
+          as="h3"
+          content="4. Freelancer starts working on the task and delivers a result"
+        />
+
+        <p>
+          The freelancer can&apos;t request money before the customer accepts
+          the job result. The freelancer has to do best to finish the task to
+          get paid.
+        </p>
+
+        <Header as="h3" content="5. Customer approves money withdrawal" />
+
+        <p>
+          The customer gets the work result and approves money withdrawal from
+          the Smart Contract by the freelancer.
+        </p>
+
+        <Header as="h3" content="6. Freelancer withdraws money" />
+
+        <p>
+          Freelancer requests money from the Smart Contract to it&apos;s own
+          crypto wallet.
         </p>
 
         <Header
@@ -108,7 +178,6 @@ export const LandingScreen = ({ isSmallScreen }) => {
             </Link>
           </List.Item>
           <List.Item>All payments in crypto</List.Item>
-          <List.Item>Remote first</List.Item>
           <List.Item>No paperwork</List.Item>
           <List.Item>No middlemen</List.Item>
         </List>
