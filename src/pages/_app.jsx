bb15e1b2d833c8156626bf48af92244427cc91ae
Alexander Kadyrov <gruz0.mail@gmail.com>
2022-11-02 10:26:19 +0400

    Send WebVitals to Google Analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,7 +4,16 @@ import { AuthProvider } from '../hooks'
 import Head from 'next/head'
 import { Web3Provider } from '../context/web3-context'
 import { DisplayProvider } from '../context/display-context'
-import { GoogleAnalytics } from 'nextjs-google-analytics'
+import { GoogleAnalytics, event } from 'nextjs-google-analytics'
+
+export function reportWebVitals({ id, name, label, value }) {
+  event(name, {
+    category: label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  })
+}
 
 function MyApp({ Component, pageProps }) {
   return (
