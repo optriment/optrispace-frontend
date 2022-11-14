e07e87c9b34cc97e211e78817105cb5be92c4346
Doğan Aydın <dogan1aydin@gmail.com>
2022-11-14 17:39:03 +0300

    removed next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.DOMAIN,
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    additionalSitemaps: [`${process.env.DOMAIN}/jobs-sitemap.xml`],
-  },
-}
